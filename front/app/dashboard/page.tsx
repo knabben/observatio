5e9978c2bfa8d4bb055556727f8a224a040611cc
Amim Knabben <amim.knabben@gmail.com>
2025-04-13 22:09:07 -0300

    Fetch cluster-info data

--- front/app/dashboard/page.tsx
+++ front/app/dashboard/page.tsx
@@ -1,18 +1,52 @@
-import { sourceCodePro400 } from '@/fonts'
+'use client';
 
-export default function Home() {
+import { sourceCodePro400 } from '@/fonts'
+import { Card, Table, Grid, Text, Title} from '@mantine/core';
+
+export default async function Home() {
+  const data = await fetch("http://localhost:8080/api/clusters/info")
+  const services = await data.json()
+
   return (
     <main>
-      <h1 className={`${sourceCodePro400.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        the middle
-        cluster summary and info
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        the other side
-      </div>
+      <Grid grow>
+        <Grid.Col span={12}>
+          <Title order={2} tt="capitalize">
+            Dashboard
+          </Title>
+        </Grid.Col>
+      </Grid>
+      <Grid grow>
+        <Grid.Col span={4}>
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Text>
+              CAPI Versions
+            </Text>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={8}>
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Table striped highlightOnHover withTableBorder withColumnBorders>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>Name</Table.Th>
+                    <Table.Th>URL</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                  {
+                    services["services"].map( (service: { name: string, path: string }, i) => (
+                      <Table.Tr key={service.name}>
+                        <Table.Td>{service.name}</Table.Td>
+                        <Table.Td><a href={service.path}>{service.path}</a></Table.Td>
+                      </Table.Tr>
+                    ))
+                  }
+                </Table.Tbody>
+              </Table>
+          </Card>
+        </Grid.Col>
+      </Grid>
     </main>
   );
 }
