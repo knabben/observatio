a8138314e63beb00c8b02a9e5b7ca519a77c4681
Amim Knabben <amim.knabben@gmail.com>
2025-04-16 12:30:30 -0300

    Adding service list test

--- webserver/internal/infra/clusterapi/cluster_test.go
+++ webserver/internal/infra/clusterapi/cluster_test.go
@@ -6,7 +6,11 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+
 	clusterctlv1 "sigs.k8s.io/cluster-api/cmd/clusterctl/api/v1alpha3"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
@@ -14,6 +18,7 @@ import (
 var (
 	scheme = runtime.NewScheme()
 	_      = clusterctlv1.AddToScheme(scheme) // Register Cluster API types
+	_      = corev1.AddToScheme(scheme)
 )
 
 func Test_GenerateCompVersions(t *testing.T) {
@@ -47,5 +52,44 @@ func Test_GenerateCompVersions(t *testing.T) {
 			assert.Equal(t, tt.provider.Version, component.Version)
 		}
 	}
+}
 
+func Test_FindServices(t *testing.T) {
+	var services corev1.ServiceList
+	tests := []struct {
+		service corev1.Service
+		config  *rest.Config
+	}{
+		{
+			service: corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "service",
+					Namespace: "kube-system",
+					Labels: map[string]string{
+						"kubernetes.io/cluster-service": "true",
+					},
+				},
+				Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+				},
+			},
+			config: &rest.Config{},
+		},
+	}
+	for _, tt := range tests {
+		var c = fake.NewClientBuilder().
+			WithScheme(scheme).
+			WithRuntimeObjects(&tt.service).
+			WithLists(&services).
+			Build()
+		ctx := context.WithValue(context.Background(), "config", tt.config)
+		services, err := FindServices(ctx, c, tt.service.ObjectMeta.Namespace)
+		assert.NoError(t, err)
+		assert.Len(t, services, 2)
+		assert.Equal(t, services[0].Name, "control-plane")
+		for _, service := range services[1:] {
+			assert.Equal(t, tt.service.Name, service.Name)
+			assert.NotEmpty(t, service.Path)
+		}
+	}
 }
