a32c70c8b9121d3b30d118519de69156b6d206c2
Amim Knabben <amim.knabben@gmail.com>
2025-04-19 10:44:03 -0300

    MachineDeployment tests

--- webserver/internal/infra/clusterapi/machinedeployment_test.go
+++ webserver/internal/infra/clusterapi/machinedeployment_test.go
@@ -1,1 +1,48 @@
 package clusterapi
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func Test_FetchMachineDeployment(t *testing.T) {
+	var machineDeploymentList clusterv1.MachineDeploymentList
+	tests := []struct {
+		d clusterv1.MachineDeployment
+	}{
+		{
+			d: clusterv1.MachineDeployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "machine-deployment",
+					Namespace: "default",
+				},
+				Spec: clusterv1.MachineDeploymentSpec{},
+				Status: clusterv1.MachineDeploymentStatus{
+					Conditions: clusterv1.Conditions{
+						{
+							Type:   "InfrastructureReady",
+							Status: corev1.ConditionTrue,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		var c = fake.NewClientBuilder().
+			WithScheme(scheme).
+			WithRuntimeObjects(&tt.d).
+			WithLists(&machineDeploymentList).
+			Build()
+		mds, err := fetchMachineDeployments(context.Background(), c)
+		assert.NoError(t, err)
+		assert.Len(t, mds, 1)
+	}
+}
