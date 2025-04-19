b7e60c97f6abcbf14b81ff34281575df8d5b65de
Amim Knabben <amim.knabben@gmail.com>
2025-04-19 19:53:11 -0300

    new logo

--- front/app/ui/dashboard/sidenav.tsx
+++ front/app/ui/dashboard/sidenav.tsx
@@ -7,15 +7,15 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-center rounded-md bg-[#A4A89C] p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-center rounded-md bg-[#A9B6A3] p-4 md:h-40"
         href="/dashboard"
       >
-        <div className="w-32 text-white md:w-40">
+        <div className="w-42 text-white md:w-40">
           <Image
             src="/logo.png"
             alt={`logo`}
-            width={200}
-            height={200}
+            width={150}
+            height={150}
           />
         </div>
       </Link>
