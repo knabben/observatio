70604fadf0e60c4dc755669f2788ca5f449179b6
Amim Knabben <amim.knabben@gmail.com>
2025-04-20 17:33:21 -0300

    Linter fixing

--- front/app/dashboard/utils.tsx
+++ front/app/dashboard/utils.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 export function FilterItems(query: string, items: any[]) {
   return items.filter((i: { name: string; }) =>
     i.name.toLowerCase().includes(query.toLowerCase()));
