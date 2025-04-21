155f8d848611845c945604868023b0e802d7fd5d
Amim Knabben <amim.knabben@gmail.com>
2025-04-21 15:18:15 -0300

    Cleaning endpoint organizations

--- webserver/internal/web/handlers/cluster.go
+++ webserver/internal/web/handlers/cluster.go
@@ -3,28 +3,7 @@ package handlers
 import (
 	"github.com/knabben/observatio/webserver/internal/infra/clusterapi"
 	"net/http"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-// handleClusterInfo returns the information about the cluster.
-func handleClusterInfo(w http.ResponseWriter, r *http.Request) {
-	var (
-		ctx       = r.Context()
-		namespace = "kube-proxy"
-		c         = ctx.Value("client").(client.Client)
-	)
-
-	services, err := clusterapi.FindServices(r.Context(), c, namespace)
-	if err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	if err = writeResponse(w, services); err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-}
 
 // handleClusterList returns the information about the cluster.
 func handleClusterList(w http.ResponseWriter, r *http.Request) {
--- webserver/internal/web/handlers/dashboard.go
+++ webserver/internal/web/handlers/dashboard.go
@@ -4,7 +4,51 @@ import (
 	"net/http"
 
 	"github.com/knabben/observatio/webserver/internal/infra/clusterapi"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+// handleComponentsVersion returns the cluster components and its versions.
+func handleComponentsVersion(w http.ResponseWriter, r *http.Request) {
+	var (
+		ctx        = r.Context()
+		components []clusterapi.Components
+	)
+
+	cli, err := clusterapi.NewClientWithScheme(ctx, scheme)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+	if components, err = clusterapi.GenerateComponentVersions(ctx, cli); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err = writeResponse(w, components); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+}
+
+// handleClusterInfo returns the information about the cluster.
+func handleClusterInfo(w http.ResponseWriter, r *http.Request) {
+	var (
+		ctx       = r.Context()
+		namespace = "kube-proxy"
+		c         = ctx.Value("client").(client.Client)
+	)
+
+	services, err := clusterapi.FindServices(r.Context(), c, namespace)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err = writeResponse(w, services); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+}
 
 // handleSummaryCluster returns the summary of clusters states.
 func handleSummaryCluster(w http.ResponseWriter, r *http.Request) {
