8422b591a83621ba47bf9dd8e5843b3c098eeeb4
Amim Knabben <amim.knabben@gmail.com>
2025-04-22 21:03:14 -0300

    Client side rendering

--- front/app/dashboard/clusters/page.tsx
+++ front/app/dashboard/clusters/page.tsx
@@ -1,36 +1,10 @@
-import Link from 'next/link';
-
-import { Suspense } from 'react';
-
-import { getClusterList } from "@/app/lib/data";
 import ClusterLister from '@/app/ui/dashboard/components/ClusterLister'
 
-import Search from '@/app/ui/dashboard/search'
-import { Grid, GridCol, Title } from '@mantine/core';
-
 export default async function Clusters() {
-  const clusters = await getClusterList()
-
   return (
     <div>
       <main>
-        <Grid justify="flex-end" align="flex-start">
-          <GridCol h={60} span={8}>
-            <Link href="/dashboard/clusters">
-              <Title className="hidden md:block" order={2}>
-                Clusters / cluster.x-k8s.io
-              </Title>
-            </Link>
-          </GridCol>
-          <GridCol span={4}>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Search placeholder="Cluster name"/>
-            </Suspense>
-          </GridCol>
-          <GridCol span={12}>
-            <ClusterLister clusterList={clusters} />
-          </GridCol>
-        </Grid>
+        <ClusterLister />
       </main>
     </div>
   )
--- front/app/ui/dashboard/components/ClusterLister.tsx
+++ front/app/ui/dashboard/components/ClusterLister.tsx
@@ -1,8 +1,13 @@
 'use client';
 
+import React, { useState, useEffect } from 'react';
+import { getClusterList } from "@/app/lib/data";
+
 import { Table } from '@mantine/core';
-import { Suspense } from 'react';
 import { Loader } from '@mantine/core';
+import Search from "@/app/ui/dashboard/search";
+import { Grid, GridCol, Title } from '@mantine/core';
+import Link from 'next/link';
 
 type Conditions = {
   type: string,
@@ -17,33 +22,52 @@ type Cluster = {
 }
 
 // ClusterLister: Cluster details and access URLs.
-export default function ClusterLister({
-  clusterList,
-}: {
-  clusterList: Cluster[] | undefined
-}) {
+export default function ClusterLister() {
+  const [clusters,setClusters] = useState<Cluster[]>([])
+  useEffect(() => {
+    const fetchData = async () => {
+      setClusters(await getClusterList())
+    }
+    fetchData().catch((e) => {
+      console.error('error', e)
+    })
+  }, [])
+
   return (
-    <Suspense fallback={<Loader />}>
-      <Table striped highlightOnHover withTableBorder withColumnBorders>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Name</Table.Th>
-            <Table.Th>ClusterClass</Table.Th>
-            <Table.Th>Conditions</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {
-            clusterList?.map( (cluster) => (
-              <Table.Tr key={cluster.name}>
-                <Table.Td>{cluster.name}</Table.Td>
-                <Table.Td>{cluster.hasTopology.toString()}</Table.Td>
-                <Table.Td>{cluster.conditions.length}</Table.Td>
-              </Table.Tr>
-            ))
-          }
-        </Table.Tbody>
-      </Table>
-    </Suspense>
+    <Grid justify="flex-end" align="flex-start">
+      <GridCol h={60} span={8}>
+        <Link href="/dashboard/clusters">
+          <Title className="hidden md:block" order={2}>
+            Clusters / cluster.x-k8s.io
+          </Title>
+        </Link>
+      </GridCol>
+      <GridCol span={4}>
+          <Search placeholder="Cluster name"/>
+      </GridCol>
+      <GridCol span={12}>
+        <Table striped highlightOnHover withTableBorder withColumnBorders>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Name</Table.Th>
+              <Table.Th>ClusterClass</Table.Th>
+              <Table.Th>Conditions</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {
+              clusters?.map( (cluster) => (
+                <Table.Tr key={cluster.name}>
+                  <Table.Td>{cluster.name}</Table.Td>
+                  <Table.Td>{cluster.hasTopology.toString()}</Table.Td>
+                  <Table.Td>{cluster.conditions.length}</Table.Td>
+                </Table.Tr>
+              ))
+            }
+          </Table.Tbody>
+        </Table>
+      </GridCol>
+    </Grid>
+
   );
 }
