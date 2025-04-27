3548c87e672a763da27dda4545d7d5be689073ac
Amim Knabben <amim.knabben@gmail.com>
2025-04-26 21:14:20 -0300

    Adding tabs extension in the frontend

--- front/app/ui/dashboard/components/clusters/table.tsx
+++ front/app/ui/dashboard/components/clusters/table.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import React from "react";
-import { Table } from '@mantine/core';
+
+import { Table, Indicator, Pill } from '@mantine/core';
 import { GridCol } from '@mantine/core';
 
-import {ClusterType} from '@/app/ui/dashboard/components/clusters/types'
+import {inter} from '@/fonts';
+import {ClusterType} from '@/app/ui/dashboard/components/clusters/types';
 
 export default function ClusterTable({
   clusters
@@ -13,12 +15,16 @@ export default function ClusterTable({
 }) {
   return (
     <GridCol span={12}>
-      <Table striped highlightOnHover withTableBorder withColumnBorders>
+      <Table striped>
         <Table.Thead>
           <Table.Tr>
             <Table.Th>Name</Table.Th>
-            <Table.Th>ClusterClass</Table.Th>
-            <Table.Th>Conditions</Table.Th>
+            <Table.Th>Paused</Table.Th>
+            <Table.Th>isClusterClass</Table.Th>
+            <Table.Th>Version</Table.Th>
+            <Table.Th>Age</Table.Th>
+            <Table.Th>Phase</Table.Th>
+            <Table.Th>Status</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>
@@ -26,7 +32,16 @@ export default function ClusterTable({
             clusters?.map( (cluster) => (
               <Table.Tr key={cluster.name}>
                 <Table.Td>{cluster.name}</Table.Td>
-                <Table.Td>{cluster.conditions.length}</Table.Td>
+                <Table.Td><Pill className={inter.className}>{cluster.paused.toString()}</Pill></Table.Td>
+                <Table.Td><Pill className={inter.className}>{cluster.clusterClass.isClusterClass.toString()}</Pill></Table.Td>
+                <Table.Td>{cluster.clusterClass.kubernetesVersion}</Table.Td>
+                <Table.Td>{cluster.created}</Table.Td>
+                <Table.Td>{cluster.phase}</Table.Td>
+                <Table.Td>
+                  {cluster.controlPlaneReady && cluster.infrastructureReady
+                    ? <Indicator inline processing color="green" size={15}/>
+                    : <Indicator inline processing color="red" size={15}/>
+                  }</Table.Td>
               </Table.Tr>
             ))
           }
--- front/app/ui/dashboard/components/clusters/types.tsx
+++ front/app/ui/dashboard/components/clusters/types.tsx
@@ -1,4 +1,4 @@
-// Cluster types and models.
+// Cluster table + details types and models.
 
 type Conditions = {
   type: string,
@@ -6,9 +6,20 @@ type Conditions = {
   lastTransitionTime: string,
 }
 
+type ClusterClass = {
+  isClusterClass: boolean,
+  machineDeployments: [],
+  kubernetesVersion: string,
+}
+
 export type ClusterType = {
   name: string,
-  hasTopology: boolean,
+  paused: boolean,
+  clusterClass: ClusterClass,
+  phase: string,
+  infrastructureReady: boolean,
+  controlPlaneReady: boolean,
+  created: string,
   conditions: Conditions[]
 }
 
