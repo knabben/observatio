1848f9b0b9b9438468bfe738ff0737e2451221d5
Amim Knabben <amim.knabben@gmail.com>
2025-04-28 17:01:29 -0300

    Spreading details

--- front/app/ui/dashboard/components/clusters/details.tsx
+++ front/app/ui/dashboard/components/clusters/details.tsx
@@ -3,6 +3,7 @@ import {Card, Grid, GridCol} from "@mantine/core";
 import { Pill, Table, Title, Indicator, Divider, Group, Space, SimpleGrid } from '@mantine/core';
 import React from "react";
 import {inter, openSans, sourceCodePro400} from "@/fonts";
+import Header from "@/app/ui/dashboard/utils/header";
 
 export default function ClusterDetails({
   cluster,
@@ -10,41 +11,95 @@ export default function ClusterDetails({
   return (
     <GridCol className={inter.className} span={12}>
       <Card className={openSans.className} withBorder shadow="sm" padding="sm" radius="md">
-          <SimpleGrid className="text-center" cols={3}>
-            <div>
-              <span className="font-bold">Cluster Name: </span>
-              {
-                cluster.controlPlaneReady && cluster.infrastructureReady
-                ? <Indicator offset={-2} inline withBorder position="top-end" color="green" size={7}> {cluster.name} </Indicator>
-                : <Indicator  offset={-2} inline withBorder position="top-end" color="red" size={7}> {cluster.name} </Indicator>
-              }
-            </div>
-            <div>
-              <span className="font-bold">Phase: </span> {cluster.phase}
-            </div>
-            <div>
-              <span className="font-bold">Age:</span> {cluster.created}
-            </div>
-          </SimpleGrid>
+        <SimpleGrid className="text-center" cols={3}>
+          <div>
+            <span className="font-bold">Cluster Name: </span>
+            {
+              cluster.controlPlaneReady && cluster.infrastructureReady
+              ? <Indicator offset={-2} inline withBorder position="top-end" color="green" size={7}> {cluster.name} </Indicator>
+              : <Indicator  offset={-2} inline withBorder position="top-end" color="red" size={7}> {cluster.name} </Indicator>
+            }
+          </div>
+          <div><span className="font-bold">Phase: </span> {cluster.phase}</div>
+          <div><span className="font-bold">Age:</span> {cluster.created}</div>
+        </SimpleGrid>
       </Card>
       <Space h="sm" />
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Grid>
-            <GridCol span={6}>
-              <Title order={4}>Specification</Title>
-              <Table className="text-lg" horizontalSpacing="sm" verticalSpacing="sm" variant="vertical">
-                <Table.Tbody>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Grid>
+          <GridCol span={6}>
+            <Header title="Specification" />
+            <Table
+              className={sourceCodePro400.className}
+              horizontalSpacing="lg"
+              verticalSpacing="sm"
+              variant="vertical">
+              <Table.Tbody className="text-base">
+                <Table.Tr>
+                  <Table.Th w={260}>Paused</Table.Th>
+                  <Table.Td><Pill size="md">{cluster.paused.toString()}</Pill></Table.Td>
+                </Table.Tr>
+                <Table.Tr>
+                  <Table.Th>Infrastructure Ready</Table.Th>
+                  <Table.Td><Pill size="md">{cluster.infrastructureReady.toString()}</Pill></Table.Td>
+                </Table.Tr>
+                <Table.Tr>
+                  <Table.Th>Control Plane Ready</Table.Th>
+                  <Table.Td><Pill size="md">{cluster.controlPlaneReady.toString()}</Pill></Table.Td>
+                </Table.Tr>
+                <Table.Tr>
+                  <Table.Th>Pod Network</Table.Th>
+                  <Table.Td>{cluster.podNetwork}</Table.Td>
+                </Table.Tr>
+                <Table.Tr>
+                  <Table.Th>Service Network</Table.Th>
+                  <Table.Td>{cluster.serviceNetwork}</Table.Td>
+                </Table.Tr>
+              </Table.Tbody>
+            </Table>
+            <Space h="lg" />
+            <Header title="Cluster conditions" />
+            <Table
+              className={sourceCodePro400.className}
+              horizontalSpacing="lg"
+              verticalSpacing="sm"
+              variant="vertical"
+              >
+              <Table.Tbody className="text-base">
+                {
+                  cluster.conditions?.map((condition) => (
+                    <Table.Tr>
+                      <Table.Th>{condition.type}</Table.Th>
+                      <Table.Td><Pill size="md">{condition.status}</Pill></Table.Td>
+                      <Table.Td>{condition.lastTransitionTime}</Table.Td>
+                    </Table.Tr>
+                  ))
+                }
+              </Table.Tbody>
+            </Table>
+        </GridCol>
+        <GridCol span={6}>
+          { cluster.clusterClass.isClusterClass ?
+            <>
+              <Header title="Cluster Class" />
+              <Table
+                className={sourceCodePro400.className}
+                horizontalSpacing="lg"
+                verticalSpacing="sm"
+                variant="vertical"
+              >
+                <Table.Tbody className="text-base">
                   <Table.Tr>
-                    <Table.Th w={160}>Paused</Table.Th>
-                    <Table.Td><Pill size="md">{cluster.paused.toString()}</Pill></Table.Td>
+                    <Table.Th className="text-medium" w={200}>Kubernetes Version</Table.Th>
+                    <Table.Td>{cluster.clusterClass.kubernetesVersion}</Table.Td>
                   </Table.Tr>
                   <Table.Tr>
-                    <Table.Th>Infrastructure Ready</Table.Th>
-                    <Table.Td><Pill size="md">{cluster.infrastructureReady.toString()}</Pill></Table.Td>
+                    <Table.Th>Cluster Class Name</Table.Th>
+                    <Table.Td>{cluster.clusterClass.className}</Table.Td>
                   </Table.Tr>
                   <Table.Tr>
                     <Table.Th>Control Plane Ready</Table.Th>
-                    <Table.Td><Pill size="md">{cluster.controlPlaneReady.toString()}</Pill></Table.Td>
+                    <Table.Td>{cluster.controlPlaneReady.toString()}</Table.Td>
                   </Table.Tr>
                   <Table.Tr>
                     <Table.Th>Pod Network</Table.Th>
@@ -56,78 +111,36 @@ export default function ClusterDetails({
                   </Table.Tr>
                 </Table.Tbody>
               </Table>
-              <Title>Cluster conditions</Title>
-              <Table horizontalSpacing="sm" verticalSpacing="sm" variant="vertical">
-                <Table.Tbody>
-                  {
-                    cluster.conditions?.map((condition) => (
-                      <Table.Tr>
-                        <Table.Th>{condition.type}</Table.Th>
-                        <Table.Td>{condition.status}</Table.Td>
-                        <Table.Td>{condition.lastTransitionTime}</Table.Td>
-                      </Table.Tr>
-                    ))
-                  }
-                </Table.Tbody>
-              </Table>
-          </GridCol>
-          <GridCol span={6}>
-            { cluster.clusterClass.isClusterClass ?
-              <>
-                <Title order={4}>Cluster class</Title>
-                <Table horizontalSpacing="sm" verticalSpacing="sm" variant="vertical">
-                  <Table.Tbody>
-                    <Table.Tr>
-                      <Table.Th w={160}>Kubernetes Version</Table.Th>
-                      <Table.Td>{cluster.clusterClass.kubernetesVersion}</Table.Td>
-                    </Table.Tr>
-                    <Table.Tr>
-                      <Table.Th>Cluster Class Name</Table.Th>
-                      <Table.Td>{cluster.clusterClass.className}</Table.Td>
-                    </Table.Tr>
-                    <Table.Tr>
-                      <Table.Th>Control Plane Ready</Table.Th>
-                      <Table.Td>{cluster.controlPlaneReady.toString()}</Table.Td>
-                    </Table.Tr>
-                    <Table.Tr>
-                      <Table.Th>Pod Network</Table.Th>
-                      <Table.Td>{cluster.podNetwork}</Table.Td>
-                    </Table.Tr>
-                    <Table.Tr>
-                      <Table.Th>Service Network</Table.Th>
-                      <Table.Td>{cluster.serviceNetwork}</Table.Td>
-                    </Table.Tr>
-                  </Table.Tbody>
-                </Table>
-              </>
-              : <div />
-            }
-            <Title>Machine Deployments</Title>
-            <Table horizontalSpacing="sm" verticalSpacing="sm">
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>Name</Table.Th>
-                  <Table.Th>Class</Table.Th>
-                  <Table.Th>Replicas</Table.Th>
-                  <Table.Th>Strategy</Table.Th>
+            </>
+            : <div />
+          }
+          <Space h="xl" />
+          <Header title="Machine Deployments" />
+          <Table horizontalSpacing="sm" verticalSpacing="sm">
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Name</Table.Th>
+                <Table.Th>Class</Table.Th>
+                <Table.Th>Replicas</Table.Th>
+                <Table.Th>Strategy</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody className="text-base">
+            {
+              cluster.clusterClass?.machineDeployments?.map((md) => (
+                <Table.Tr className={sourceCodePro400.className} key={cluster.name}>
+                  <Table.Td>{md.name}</Table.Td>
+                  <Table.Td>{md.class}</Table.Td>
+                  <Table.Td>{md.replicas}</Table.Td>
+                  <Table.Td>{md.strategy?.type}</Table.Td>
                 </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody className="text-base">
-              {
-                cluster.clusterClass?.machineDeployments?.map((md) => (
-                  <Table.Tr className={sourceCodePro400.className} key={cluster.name}>
-                    <Table.Td>{md.name}</Table.Td>
-                    <Table.Td>{md.class}</Table.Td>
-                    <Table.Td>{md.replicas}</Table.Td>
-                    <Table.Td>{md.strategy?.type}</Table.Td>
-                  </Table.Tr>
-                ))
-              }
-              </Table.Tbody>
-            </Table>
-          </GridCol>
-        </Grid>
-      </Card>
-    </GridCol>
+              ))
+            }
+            </Table.Tbody>
+          </Table>
+        </GridCol>
+      </Grid>
+    </Card>
+  </GridCol>
   )
 }
