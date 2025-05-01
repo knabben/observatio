2c84d34cfa0b89417ee49e48be8f7011f0f2599b
TKG Release <tkg-release@broadcom.net>
2025-05-01 19:27:26 -0300

    Fixing tables for mds

--- front/app/ui/dashboard/components/mds/table.tsx
+++ front/app/ui/dashboard/components/mds/table.tsx
@@ -1,37 +1,46 @@
 'use client';
 
 import React from "react";
-import { Table } from '@mantine/core';
+import { Table, Indicator } from '@mantine/core';
 import { GridCol } from '@mantine/core';
 
-type MachineDeployment = {
-  name: string,
-  namespace: string,
-  phase: string,
-}
+import {MachineDeploymentType} from '@/app/ui/dashboard/components/mds/types';
 
 export default function MDTable({
   mds,
 }: {
-  mds: MachineDeployment[]
+  mds: MachineDeploymentType[]
 }) {
   return (
     <GridCol span={12}>
-    <Table striped highlightOnHover withTableBorder withColumnBorders>
+    <Table highlightOnHover>
       <Table.Thead>
         <Table.Tr>
           <Table.Th>Name</Table.Th>
           <Table.Th>Namespace</Table.Th>
-          <Table.Th>Phase</Table.Th>
+          <Table.Th>Replicas</Table.Th>
+          <Table.Th>Cluster</Table.Th>
+          <Table.Th>Age</Table.Th>
+          <Table.Th ta="center">Phase</Table.Th>
+          <Table.Th ta="center">Status</Table.Th>
         </Table.Tr>
       </Table.Thead>
-      <Table.Tbody>
+      <Table.Tbody className="text-sm">
         {
           mds.map((md, i) => (
             <Table.Tr key={i}>
               <Table.Td>{md.name}</Table.Td>
               <Table.Td>{md.namespace}</Table.Td>
-              <Table.Td>{md.phase}</Table.Td>
+              <Table.Td>{md.replicas}</Table.Td>
+              <Table.Td>{md.cluster}</Table.Td>
+              <Table.Td>{md.created}</Table.Td>
+              <Table.Td ta="center">{md.phase}</Table.Td>
+              <Table.Td ta="center">
+                {md.unavailableReplicas == 0
+                  ? <Indicator inline processing color="green" size={15}/>
+                  : <Indicator inline processing color="red" size={15}/>
+                }
+              </Table.Td>
             </Table.Tr>
           ))
         }
