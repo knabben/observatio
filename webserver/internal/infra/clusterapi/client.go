12c5555a9cc64e55697fa5cf1788ff9ad0ba7972
Amim Knabben <amim.knabben@gmail.com>
2025-05-04 11:52:52 -0300

    newdynamicclient addition

--- webserver/internal/infra/clusterapi/client.go
+++ webserver/internal/infra/clusterapi/client.go
@@ -2,13 +2,14 @@ package clusterapi
 
 import (
 	"context"
-
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
+// NewClient returns a client and configuration for cluster accessibility.
 func NewClient() (client.Client, *rest.Config, error) {
 	cfg := config.GetConfigOrDie()
 	cli, err := client.New(cfg, client.Options{})
@@ -18,7 +19,13 @@ func NewClient() (client.Client, *rest.Config, error) {
 	return cli, cfg, nil
 }
 
+// NewClientWithScheme returns the client with scheme.
 func NewClientWithScheme(ctx context.Context, scheme *runtime.Scheme) (client.Client, error) {
 	cfg := ctx.Value("config").(*rest.Config)
 	return client.New(cfg, client.Options{Scheme: scheme})
+}
+
+// NewDynamicClient returns a dynamic client for unstructured object access.
+func NewDynamicClient(ctx context.Context) (*dynamic.DynamicClient, error) {
+	return dynamic.NewForConfig(ctx.Value("config").(*rest.Config))
 }
