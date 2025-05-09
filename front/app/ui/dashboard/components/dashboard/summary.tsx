7feae75d97399845b02402be35488233df48c0c9
Amim Knabben <amim.knabben@gmail.com>
2025-05-08 22:20:58 -0300

    Clean summary and giving space for new component

--- front/app/ui/dashboard/components/dashboard/summary.tsx
+++ front/app/ui/dashboard/components/dashboard/summary.tsx
@@ -1,23 +1,33 @@
 'use client';
 
 import React, {useState, useEffect} from 'react';
-import {Card, Grid, Space, Text} from '@mantine/core';
+import {Card, Grid} from '@mantine/core';
 import {getClusterSummary} from "@/app/lib/data";
 import Header from "@/app/ui/dashboard/utils/header";
 import {sourceCodePro400} from "@/fonts";
+import {RadialBarChart, PieChart} from "@mantine/charts";
+
 
 type Summary = {
-  failed?: bigint
-  provisioned?: bigint,
+  name: string,
+  value: number,
+  color: string,
 }
 
-// Summary : Resume clusters statuses.
+/**
+ * ClusterSummary is a React functional component that displays a summary of cluster health
+ * statistics in a card layout. It fetches cluster summary data, including the number of
+ * running and failed clusters, and presents the data both in textual and graphical formats.
+ */
 export default function ClusterSummary() {
-  const [clusterSummary, setClusterSummary] = useState<Summary>({});
-
+  const [summary, setSummary] = useState<Summary[]>([]);
   useEffect( () => {
     const fetch = async  () => {
-      setClusterSummary(await getClusterSummary())
+      const response = await getClusterSummary()
+      setSummary([
+        { name: 'Running', value: response.provisioned, color: '#39b69d' },
+        { name: 'Failed', value: response.failed, color: '#f53f5e' },
+      ])
     }
     fetch().catch( (e) => { console.error('error', e) })
   }, [])
@@ -25,20 +35,18 @@ export default function ClusterSummary() {
   return (
     <Card shadow="md" className={sourceCodePro400.className} padding="lg" radius="md" withBorder>
       <Header title="Clusters Health"/>
-      <div style={{ resize: 'vertical', overflow: 'hidden', maxHeight: '100%' }}>
-        <Grid ta="center">
-          <Grid.Col span={6}>
-            <Text size="lg">Provisioned</Text>
-            <Space h="lg" />
-            <div className="text-5xl text-[#39b69d]">{clusterSummary.provisioned}</div>
-          </Grid.Col>
-          <Grid.Col span={6}>
-            Failing
-            <Space h="lg"/>
-            <div className="text-5xl text-[#e01003]">{clusterSummary.failed}</div>
-          </Grid.Col>
-        </Grid>
-      </div>
+      <Grid justify="center" align="center" ta="center">
+        <Grid.Col span={6}>
+          {
+            summary?.map((summary: Summary, i: number) => (
+              <div key={i}>{summary.value} {summary.name}</div>
+            ))
+           }
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <RadialBarChart data={summary} dataKey="value" h={250} />
+        </Grid.Col>
+      </Grid>
     </Card>
   );
 }
