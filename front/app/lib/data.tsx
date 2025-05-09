70bc0f17cd9ea464ebc026456f70d28109df39f6
Amim Knabben <amim.knabben@gmail.com>
2025-05-09 19:51:19 -0300

    Padding on chips components

--- front/app/lib/data.tsx
+++ front/app/lib/data.tsx
@@ -1,4 +1,4 @@
-export const URL = typeof window !== 'undefined' ? `http://${window.location.hostname}:8080/` : 'http://localhost:8080/';
+export const URL = typeof window !== 'undefined' ? `http://${window.location.hostname}:8080` : 'http://localhost:8080';
 
 // ----- Dashboard -----
 
