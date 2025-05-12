c7066c1f4eb46883b60fb3f45b2ffdebaa19b444
Amim Knabben <amim.knabben@gmail.com>
2025-05-12 13:10:44 -0300

    Endpoint handlers for machine infra

--- webserver/internal/infra/clusterapi/processor/machine.go
+++ webserver/internal/infra/clusterapi/processor/machine.go
@@ -4,10 +4,13 @@ import (
 	"time"
 
 	"github.com/knabben/observatio/webserver/internal/infra/models"
+	capv "sigs.k8s.io/cluster-api-provider-vsphere/apis/v1beta1"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
-// ProcessMachine returns the list of Machines objects from the mgmt cluster.
+// ProcessMachine returns a Machine object with details extracted from the provided Machine object, including
+// machine name, namespace, owner, cluster name, node name, provider ID, version, bootstrap configuration,
+// bootstrap and infrastructure readiness, creation time, and machine phase.
 func ProcessMachine(m clusterv1.Machine) models.Machine {
 	var nodeRef string
 	if m.Status.NodeRef != nil {
@@ -65,6 +68,55 @@ func ProcessMachineResponse(machines []clusterv1.Machine) models.MachineResponse
 	}
 }
 
+// isMachineFailed checks if the machine has failed by evaluating infrastructure and bootstrap readiness.
 func isMachineFailed(machine clusterv1.Machine) bool {
 	return !machine.Status.InfrastructureReady || !machine.Status.BootstrapReady
+}
+
+// ProcessMachineInfra maps a capv.VSphereMachine object to a models.MachineInfra object
+// extracting relevant infrastructure details such as name, namespace, provider ID, memory,
+// disk, failure information, etc.
+func ProcessMachineInfra(machine capv.VSphereMachine) models.MachineInfra {
+	return models.MachineInfra{
+		Name:              machine.Name,
+		Namespace:         machine.Namespace,
+		ProviderID:        stringPointer(machine.Spec.ProviderID),
+		FailureDomain:     stringPointer(machine.Spec.FailureDomain),
+		PowerOffMode:      machine.Spec.PowerOffMode,
+		Template:          machine.Spec.Template,
+		CloneMode:         machine.Spec.CloneMode,
+		NumCPUs:           machine.Spec.NumCPUs,
+		NumCoresPerSocket: machine.Spec.NumCoresPerSocket,
+		MemoryMiB:         machine.Spec.MemoryMiB,
+		DiskGiB:           machine.Spec.DiskGiB,
+		Ready:             machine.Status.Ready,
+		FailureReason:     machine.Status.FailureReason,
+		FailureMessage:    stringPointer(machine.Status.FailureMessage),
+		Conditions:        machine.Status.Conditions,
+	}
+}
+
+// ProcessMachineInfraResponse processes a list of VSphereMachines and returns MachineInfraResponse.
+func ProcessMachineInfraResponse(machines []capv.VSphereMachine) models.MachineInfraResponse {
+	failed := 0
+	machineList := make([]models.MachineInfra, 0, len(machines))
+	for _, m := range machines {
+		machineList = append(machineList, ProcessMachineInfra(m))
+		if !m.Status.Ready {
+			failed++
+		}
+	}
+	return models.MachineInfraResponse{
+		Total:    len(machines),
+		Machines: machineList,
+		Failing:  failed,
+	}
+}
+
+// stringPointer returns an empty string if the input pointer is nil, otherwise returns the dereferenced string.
+func stringPointer(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
 }
