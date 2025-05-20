5b179297f2f5a21dfb36d547ee426c98f7c23ee7
Amim Knabben <amim.knabben@gmail.com>
2025-05-20 10:35:55 -0300

    Improving prompt eng.

--- webserver/internal/infra/llm/messages.go
+++ webserver/internal/infra/llm/messages.go
@@ -2,17 +2,41 @@ package llm
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/anthropics/anthropic-sdk-go"
 	"github.com/knabben/observatio/webserver/internal/infra/models"
 )
 
+var (
+	TASK_SYSTEM = `
+		You will serve as a Kubernetes administrator managing an on-premises datacenter using VMware vCenter.
+	`
+	TASK_CONTEXT = `
+		Your task is to assist operators in troubleshooting issues within the cluster.
+		You should maintain a friendly customer service tone.
+	`
+	INPUT_DATA  = `Here is the customer question: <question> %s </question>`
+	OUTPUT_DATA = `The answer must be divided in two parts:
+		A verbose description of the error in <description></description> tags
+		The solution of the error in <solution></solution> tags`
+)
+
+func (c *AnthropicClient) formatMessage(msg string) string {
+	data := fmt.Sprintf(INPUT_DATA, msg)
+	return fmt.Sprintf("%s\n%s\n%s", TASK_CONTEXT, data, OUTPUT_DATA)
+}
+
 func (c *AnthropicClient) SendMessage(ctx context.Context) (response models.LLMResponse, err error) {
 	var msg *anthropic.Message
+
 	msg, err = c.Client.Messages.New(ctx, anthropic.MessageNewParams{
 		MaxTokens: 1024,
 		Messages: []anthropic.MessageParam{
-			anthropic.NewUserMessage(anthropic.NewTextBlock(c.Error)),
+			anthropic.NewUserMessage(anthropic.NewTextBlock(c.formatMessage(c.Error))),
+		},
+		System: []anthropic.TextBlockParam{
+			{Text: TASK_SYSTEM},
 		},
 		Model:         anthropic.ModelClaude3_7SonnetLatest,
 		StopSequences: []string{"---\n"},
