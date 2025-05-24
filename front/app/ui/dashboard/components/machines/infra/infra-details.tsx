2ee263dc1177d49e9ad7dae0a247d10e778da9f9
Amim Knabben <amim.knabben@gmail.com>
2025-05-24 11:50:31 -0300

    Base components and machine infra details

--- front/app/ui/dashboard/components/machines/infra/infra-details.tsx
+++ front/app/ui/dashboard/components/machines/infra/infra-details.tsx
@@ -1,71 +1,65 @@
-import {MachineInfraType} from "@/app/ui/dashboard/components/machines/types";
-import {roboto} from "@/fonts";
 import React from "react";
+import {MachineInfraType} from "@/app/ui/dashboard/components/machines/types";
 import {
   Text,
-  Card,
-  GridCol,
   Group,
   SimpleGrid,
-  Space,
   Stack,
-  Tabs,
-  TabsPanel
 } from "@mantine/core";
 import {IconCheck, IconX} from "@tabler/icons-react";
-import AITroubleshooting from "@/app/ui/dashboard/components/machines/infra/ai-troubleshooting";
+import AITroubleshooting from "@/app/ui/dashboard/base/ai-troubleshooting";
 import Specification from "@/app/ui/dashboard/components/machines/infra/specification";
+import ObjectDetails from "@/app/ui/dashboard/base/details";
 
 export default function MachineInfraDetails({
   machine
 }: {machine: MachineInfraType}) {
+  const tabs = [
+    {
+      label: "Specification",
+      content: (machine: MachineInfraType) => <Specification machine={machine} />
+    },
+    {
+      label: "AI Troubleshooting",
+      content: (machine: MachineInfraType) => <AITroubleshooting conditions={machine.status.conditions} />
+    }];
+
+  const headerRender = (machine: MachineInfraType) => (
+    <SimpleGrid cols={2}>
+      <div className="flex items-center h-full">
+        <Group justify="flex-start">
+          {
+            machine.status.ready
+              ? <IconCheck size={40} color="teal"/>
+              : <IconX color="red" size={40}/>
+          }
+          <Text className="text-bold" fw={700}>{machine.metadata?.name}</Text>
+        </Group>
+      </div>
+      <div>
+        <Group justify="flex-end">
+          <Stack gap="sm" justify="center">
+            <Text size="sm">Namespace</Text>
+            <Text size="xl">
+              {machine.metadata?.namespace}
+            </Text>
+          </Stack>
+          <Stack gap="sm" justify="center">
+            <Text size="sm">Created</Text>
+            <Text size="xl">
+              {machine.age}
+            </Text>
+          </Stack>
+        </Group>
+      </div>
+    </SimpleGrid>
+  );
+
   return (
-      <GridCol className={roboto.className} span={12}>
-        <Card withBorder shadow="sm" padding="md" radius="md">
-          <SimpleGrid cols={2}>
-            <div className="flex items-center h-full">
-              <Group justify="flex-start">
-                {
-                  machine.status.ready
-                    ? <IconCheck size={40} color="teal"/>
-                    : <IconX color="red" size={40}/>
-                }
-                <Text className="text-bold" fw={700}>{machine.metadata?.name}</Text>
-              </Group>
-            </div>
-            <div>
-            <Group justify="flex-end">
-                <Stack gap="sm" justify="center">
-                  <Text size="sm">Namespace</Text>
-                  <Text size="xl">
-                    {machine.metadata?.namespace}
-                  </Text>
-                </Stack>
-                <Stack gap="sm" justify="center">
-                  <Text size="sm">Created</Text>
-                  <Text size="xl">
-                    {machine.age}
-                  </Text>
-                </Stack>
-            </Group>
-            </div>
-          </SimpleGrid>
-        </Card>
-        <Space h="md" />
-        <Tabs mb="md" color="#48654a" defaultValue="spec">
-          <Tabs.List>
-            <Tabs.Tab value="spec">Specification</Tabs.Tab>
-            <Tabs.Tab value="troubleshooting">AI Troubleshooting</Tabs.Tab>
-          </Tabs.List>
-        <TabsPanel value="spec">
-          <Space h="lg" />
-          <Specification machine={machine} />
-        </TabsPanel>
-        <TabsPanel value="troubleshooting">
-          <Space h="lg" />
-          <AITroubleshooting conditions={machine.status.conditions} />
-        </TabsPanel>
-        </Tabs>
-      </GridCol>
+    <ObjectDetails
+      object={machine}
+      headerRenderer={headerRender}
+      tabs={tabs}
+    />
   )
 }
