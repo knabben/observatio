5baaa5385cac78568d4e04bc50555756719e400a
Amim Knabben <amim.knabben@gmail.com>
2025-05-24 11:21:19 -0300

    Fix md lister

--- front/app/ui/dashboard/components/mds/lister.tsx
+++ front/app/ui/dashboard/components/mds/lister.tsx
@@ -1,24 +1,12 @@
 'use client';
 
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
-import {sourceCodePro400} from "@/fonts";
-
-import Search from '@/app/ui/dashboard/search'
-import {FilterItems} from "@/app/dashboard/utils";
-import { Grid, GridCol, Title } from '@mantine/core';
+import React from 'react';
 
 import MachineDeploymentTable from '@/app/ui/dashboard/components/mds/table'
 import MachineDeploymentDetails from "@/app/ui/dashboard/components/mds/details";
 
-import {receiveAndPopulate, sendInitialRequest, WebSocket} from "@/app/lib/websocket";
-import {CenteredLoader} from "@/app/ui/dashboard/utils/loader";
 import {MachineDeploymentType} from "@/app/ui/dashboard/components/mds/types";
 import BaseLister from "@/app/ui/dashboard/base/lister";
-import {MachineType} from "@/app/ui/dashboard/components/machines/types";
-import MachineDetails from "@/app/ui/dashboard/components/machines/details";
-import MachinesTable from "@/app/ui/dashboard/components/machines/table";
-
 /**
  * MachineDeploymentLister is a functional component responsible for listing machine deployments
  * and optionally showing details for a selected deployment. It manages state for the deployments,
@@ -30,7 +18,7 @@ export default function MachineDeploymentLister() {
     items={[]}
     renderDetails={(item: MachineDeploymentType) => <MachineDeploymentDetails md={item}/>}
     renderTable={(items : MachineDeploymentType[], handleSelect) =>  (
-      <MachineDeploymentTable  mds={items}/>
+      <MachineDeploymentTable select={handleSelect} mds={items}/>
     )}
     title="Machine Deployments / cluster.x-k8s.io"
   />
--- front/app/ui/dashboard/components/mds/types.tsx
+++ front/app/ui/dashboard/components/mds/types.tsx
@@ -1,14 +1,44 @@
 // MachineDeployment models and details
 
+import {Conditions, MachineMeta} from "@/app/ui/dashboard/components/machines/types";
+
 export type MachineDeploymentType = {
-  name: string,
+  metadata: MachineMeta,
   replicas: number,
-  namespace: string,
   cluster: string,
+  age: string,
+  templateversion: string,
+  templateBootstrap: Bootstrap,
+  templateInfrastructureRef: InfrastructureRef,
+  status: MachineDeploymentStatus,
+}
+
+export type Bootstrap = {
+  configRef: {
+    name: string,
+    namespace: string,
+    kind: string,
+    apiVersion: string,
+  }
+}
+
+export type InfrastructureRef = {
+  name: string,
+  namespace: string,
+  kind: string,
+  apiVersion: string,
+}
+
+export type MachineDeploymentStatus = {
+  phase: string,
+  condition: Conditions[],
   readyReplicas: number,
   updatedReplicas: number,
   unavailableReplicas: number,
-  created: string
-  phase: string,
+  v1beta2: {
+    availableReplicas: number,
+    readyReplicas: number,
+    upToDateReplicas: number,
+    conditions: Conditions[],
+  }
 }
-
